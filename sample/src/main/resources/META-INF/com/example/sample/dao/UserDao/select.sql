8fb41d2a99adf15bdfc04d9ab2c750e41993844f
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-08-25 13:59:06 +0900

    add: UserDao SQLテンプレート

---
+++ sample/src/main/resources/META-INF/com/example/sample/dao/UserDao/select.sql
@@ -0,0 +1,22 @@
+SELECT
+    user_id
+    ,name
+    ,email
+    ,password
+    ,created_by
+    ,created_at
+    ,updated_by
+    ,updated_at
+    ,deleted_by
+    ,deleted_at
+    ,version
+FROM
+    users
+WHERE
+    deleted_at IS NULL
+/*%if criteria.id != null */
+AND user_id = /* criteria.id */1
+/*%end*/
+/*%if criteria.email != null */
+AND email = /* criteria.email */'kuroda@example.com'
+/*%end*/

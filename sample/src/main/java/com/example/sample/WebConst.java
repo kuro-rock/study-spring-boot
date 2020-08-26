c77490388b6d441634ee20d97f435511f1328012
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-08-26 09:13:57 +0900

    add: constファイル

---
+++ sample/src/main/java/com/example/sample/WebConst.java
@@ -0,0 +1,40 @@
+package com.example.sample;
+
+public interface WebConst {
+
+    /** ---- MDC ---- **/
+    String MDC_LOGIN_USER_ID = "LOGIN_USER_ID";
+
+    String MDC_FUNCTION_NAME = "FUNCTION_NAME";
+
+    /** ---- URLs ---- **/
+    String HOME_URL = "/";
+
+    String ERROR_URL = "/error";
+
+    String FORBIDDEN_URL = "/forbidden";
+
+    String LOGIN_URL = "/login";
+
+    String LOGIN_PROCESSING_URL = "/authenticate";
+
+    String LOGIN_SUCCESS_URL = "/loginSuccess";
+
+    String LOGIN_FAILURE_URL = "/loginFailure";
+
+    String LOGIN_TIMEOUT_URL = "/loginTimeout";
+
+    String RESET_PASSWORD_URL = "/resetPassword";
+
+    String CHANGE_PASSWORD_URL = "/changePassword";
+
+    String LOGOUT_URL = "/logout";
+
+    String LOGOUT_SUCCESS_URL = "/logoutSuccess";
+
+    String WEBJARS_URL = "/webjars/**";
+
+    String STATIC_RESOURCES_URL = "/static/**";
+
+    String API_BASE_URL = "/api/**";
+}
